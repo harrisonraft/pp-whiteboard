b88b7d7e744d35bcbabe4f15a16d2b46a33b4290
Harrison Smith <harrison@raftdigital.co.uk>
2025-02-20 16:22:33 +0000

    remove rollup cache

--- .rollup.cache/Users/harrisonsmith/Projects/Pebble/guild/whiteboard/src/whiteboard-canvas/whiteboard-canvas.component.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-export var WhiteboardCanvas = function () {
-    return React.createElement("canvas", { width: "1000", height: "1000" });
-};
-//# sourceMappingURL=whiteboard-canvas.component.js.map
